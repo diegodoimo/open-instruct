a0533f7bf0acf4b5d4f2259ce1b4b10fdda20609
DiegoD <doimo.diego@gmail.com>
2024-04-18 12:03:52 +0200

    reset dataset

--- open_instruct/finetune.py
+++ open_instruct/finetune.py
@@ -33,7 +33,7 @@ from transformers import (
 )
 from peft import LoraConfig, TaskType, get_peft_model, prepare_model_for_kbit_training
 
-# from data_utils import get_mmlu_open_instruct, DataCollatorForCausalLM
+from data_utils import get_mmlu_open_instruct_old, DataCollatorForCausalLM
 import numpy as np
 import sys
 import time
@@ -327,114 +327,114 @@ def parse_args():
 #     sys.stdout.flush()
 
 
-# def encode_with_prompt_completion_format(example, tokenizer, max_seq_length):
-#     """
-#     Here we assume each example has 'prompt' and 'completion' fields.
-#     We concatenate prompt and completion and tokenize them together because otherwise prompt will be padded/trancated
-#     and it doesn't make sense to follow directly with the completion.
-#     """
-#     # if prompt doesn't end with space and completion doesn't start with space, add space
-#     if not example["prompt"].endswith((" ", "\n", "\t")) and not example[
-#         "completion"
-#     ].startswith((" ", "\n", "\t")):
-#         example_text = example["prompt"] + " " + example["completion"]
-#     else:
-#         example_text = example["prompt"] + example["completion"]
-#     example_text = example_text + tokenizer.eos_token
-#     tokenized_example = tokenizer(
-#         example_text, return_tensors="pt", max_length=max_seq_length, truncation=True
-#     )
-#     input_ids = tokenized_example.input_ids
-#     labels = input_ids.clone()
-#     tokenized_prompt = tokenizer(
-#         example["prompt"],
-#         return_tensors="pt",
-#         max_length=max_seq_length,
-#         truncation=True,
-#     )
-#     # mask the prompt part for avoiding loss
-#     labels[:, : tokenized_prompt.input_ids.shape[1]] = -100
-#     attention_mask = torch.ones_like(input_ids)
-#     return {
-#         "input_ids": input_ids.flatten(),
-#         "labels": labels.flatten(),
-#         "attention_mask": attention_mask.flatten(),
-#     }
+def encode_with_prompt_completion_format(example, tokenizer, max_seq_length):
+    """
+    Here we assume each example has 'prompt' and 'completion' fields.
+    We concatenate prompt and completion and tokenize them together because otherwise prompt will be padded/trancated
+    and it doesn't make sense to follow directly with the completion.
+    """
+    # if prompt doesn't end with space and completion doesn't start with space, add space
+    if not example["prompt"].endswith((" ", "\n", "\t")) and not example[
+        "completion"
+    ].startswith((" ", "\n", "\t")):
+        example_text = example["prompt"] + " " + example["completion"]
+    else:
+        example_text = example["prompt"] + example["completion"]
+    example_text = example_text + tokenizer.eos_token
+    tokenized_example = tokenizer(
+        example_text, return_tensors="pt", max_length=max_seq_length, truncation=True
+    )
+    input_ids = tokenized_example.input_ids
+    labels = input_ids.clone()
+    tokenized_prompt = tokenizer(
+        example["prompt"],
+        return_tensors="pt",
+        max_length=max_seq_length,
+        truncation=True,
+    )
+    # mask the prompt part for avoiding loss
+    labels[:, : tokenized_prompt.input_ids.shape[1]] = -100
+    attention_mask = torch.ones_like(input_ids)
+    return {
+        "input_ids": input_ids.flatten(),
+        "labels": labels.flatten(),
+        "attention_mask": attention_mask.flatten(),
+    }
 
 
-# def encode_with_messages_format(example, tokenizer, max_seq_length):
-#     """
-#     Here we assume each example has a 'messages' field Each message is a dict with 'role' and 'content' fields.
-#     We concatenate all messages with the roles as delimiters and tokenize them together.
-#     """
-#     messages = example["messages"]
-#     if len(messages) == 0:
-#         raise ValueError("messages field is empty.")
+def encode_with_messages_format(example, tokenizer, max_seq_length):
+    """
+    Here we assume each example has a 'messages' field Each message is a dict with 'role' and 'content' fields.
+    We concatenate all messages with the roles as delimiters and tokenize them together.
+    """
+    messages = example["messages"]
+    if len(messages) == 0:
+        raise ValueError("messages field is empty.")
 
-#     def _concat_messages(messages):
-#         message_text = ""
-#         for message in messages:
-#             if message["role"] == "system":
-#                 message_text += "<|system|>\n" + message["content"].strip() + "\n"
-#             elif message["role"] == "user":
-#                 message_text += "<|user|>\n" + message["content"].strip() + "\n"
-#             elif message["role"] == "assistant":
-#                 message_text += (
-#                     "<|assistant|>\n"
-#                     + message["content"].strip()
-#                     + tokenizer.eos_token
-#                     + "\n"
-#                 )
-#             else:
-#                 raise ValueError("Invalid role: {}".format(message["role"]))
-#         return message_text
+    def _concat_messages(messages):
+        message_text = ""
+        for message in messages:
+            if message["role"] == "system":
+                message_text += "<|system|>\n" + message["content"].strip() + "\n"
+            elif message["role"] == "user":
+                message_text += "<|user|>\n" + message["content"].strip() + "\n"
+            elif message["role"] == "assistant":
+                message_text += (
+                    "<|assistant|>\n"
+                    + message["content"].strip()
+                    + tokenizer.eos_token
+                    + "\n"
+                )
+            else:
+                raise ValueError("Invalid role: {}".format(message["role"]))
+        return message_text
 
-#     example_text = _concat_messages(messages).strip()
-#     tokenized_example = tokenizer(
-#         example_text, return_tensors="pt", max_length=max_seq_length, truncation=True
-#     )
-#     input_ids = tokenized_example.input_ids
-#     labels = input_ids.clone()
+    example_text = _concat_messages(messages).strip()
+    tokenized_example = tokenizer(
+        example_text, return_tensors="pt", max_length=max_seq_length, truncation=True
+    )
+    input_ids = tokenized_example.input_ids
+    labels = input_ids.clone()
 
-#     # mask the non-assistant part for avoiding loss
-#     for message_idx, message in enumerate(messages):
-#         if message["role"] != "assistant":
-#             if message_idx == 0:
-#                 message_start_idx = 0
-#             else:
-#                 message_start_idx = tokenizer(
-#                     _concat_messages(messages[:message_idx]),
-#                     return_tensors="pt",
-#                     max_length=max_seq_length,
-#                     truncation=True,
-#                 ).input_ids.shape[1]
-#             if (
-#                 message_idx < len(messages) - 1
-#                 and messages[message_idx + 1]["role"] == "assistant"
-#             ):
-#                 # here we also ignore the role of the assistant
-#                 messages_so_far = (
-#                     _concat_messages(messages[: message_idx + 1]) + "<|assistant|>\n"
-#                 )
-#             else:
-#                 messages_so_far = _concat_messages(messages[: message_idx + 1])
-#             message_end_idx = tokenizer(
-#                 messages_so_far,
-#                 return_tensors="pt",
-#                 max_length=max_seq_length,
-#                 truncation=True,
-#             ).input_ids.shape[1]
-#             labels[:, message_start_idx:message_end_idx] = -100
+    # mask the non-assistant part for avoiding loss
+    for message_idx, message in enumerate(messages):
+        if message["role"] != "assistant":
+            if message_idx == 0:
+                message_start_idx = 0
+            else:
+                message_start_idx = tokenizer(
+                    _concat_messages(messages[:message_idx]),
+                    return_tensors="pt",
+                    max_length=max_seq_length,
+                    truncation=True,
+                ).input_ids.shape[1]
+            if (
+                message_idx < len(messages) - 1
+                and messages[message_idx + 1]["role"] == "assistant"
+            ):
+                # here we also ignore the role of the assistant
+                messages_so_far = (
+                    _concat_messages(messages[: message_idx + 1]) + "<|assistant|>\n"
+                )
+            else:
+                messages_so_far = _concat_messages(messages[: message_idx + 1])
+            message_end_idx = tokenizer(
+                messages_so_far,
+                return_tensors="pt",
+                max_length=max_seq_length,
+                truncation=True,
+            ).input_ids.shape[1]
+            labels[:, message_start_idx:message_end_idx] = -100
 
-#             if message_end_idx >= max_seq_length:
-#                 break
+            if message_end_idx >= max_seq_length:
+                break
 
-#     attention_mask = torch.ones_like(input_ids)
-#     return {
-#         "input_ids": input_ids.flatten(),
-#         "labels": labels.flatten(),
-#         "attention_mask": attention_mask.flatten(),
-#     }
+    attention_mask = torch.ones_like(input_ids)
+    return {
+        "input_ids": input_ids.flatten(),
+        "labels": labels.flatten(),
+        "attention_mask": attention_mask.flatten(),
+    }
 
 
 # def save_with_accelerate(accelerator, model, tokenizer, output_dir, args):
@@ -503,23 +503,6 @@ def main():
 
     # *******************************************************
     # ********************************************************
-
-    # if args.dataset_name is not None:
-    #     # Downloading and loading a dataset from the hub.
-    #     raw_datasets = load_dataset(
-    #         args.dataset_name,
-    #         args.dataset_config_name,
-    #     )
-    # else:
-    #     data_files = {}
-    #     dataset_args = {}
-    #     if args.train_file is not None:
-    #         data_files["train"] = args.train_file
-    #     raw_datasets = load_dataset(
-    #         "json",
-    #         data_files=data_files,
-    #         **dataset_args,
-    #     )
 
     # Load pretrained model and tokenizer
     if args.config_name:
@@ -653,56 +636,6 @@ def main():
         model = get_peft_model(model, peft_config)
         model.print_trainable_parameters()
 
-    # Preprocessing the datasets.
-    # print("start preprocessing the data. \n\n")
-    # sys.stdout.flush()
-    # if (
-    #     "prompt" in raw_datasets["train"].column_names
-    #     and "completion" in raw_datasets["train"].column_names
-    # ):
-    #     encode_function = partial(
-    #         encode_with_prompt_completion_format,
-    #         tokenizer=tokenizer,
-    #         max_seq_length=args.max_seq_length,
-    #     )
-    # elif "messages" in raw_datasets["train"].column_names:
-    #     encode_function = partial(
-    #         encode_with_messages_format,
-    #         tokenizer=tokenizer,
-    #         max_seq_length=args.max_seq_length,
-    #     )
-    # else:
-    #     raise ValueError(
-    #         "You need to have either 'prompt'&'completion' or 'messages' in your column names."
-    #     )
-
-    # with accelerator.main_process_first():
-    #     lm_datasets = raw_datasets.map(
-    #         encode_function,
-    #         batched=False,
-    #         num_proc=args.preprocessing_num_workers,
-    #         load_from_cache_file=not args.overwrite_cache,
-    #         remove_columns=[
-    #             name
-    #             for name in raw_datasets["train"].column_names
-    #             if name not in ["input_ids", "labels", "attention_mask"]
-    #         ],
-    #         desc="Tokenizing and reformatting instruction data",
-    #     )
-
-    #     lm_datasets.set_format(type="pt")
-    #     lm_datasets = lm_datasets.filter(
-    #         lambda example: (example["labels"] != -100).any()
-    #     )
-
-    # train_dataset = lm_datasets["train"]
-    # print("finished preprocessing. \n\n")
-    # sys.stdout.flush()
-
-    # # Log a few random samples from the training set:
-    # # for index in random.sample(range(len(train_dataset)), 3):
-    # #    logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
-
     # # DataLoaders creation:
     # train_dataloader = DataLoader(
     #     train_dataset,
@@ -715,34 +648,12 @@ def main():
     #     ),
     #     batch_size=args.per_device_train_batch_size,
     # )
-
     # print(len(train_dataloader))
-
-    # val_dataset = get_mmlu_open_instruct(
-    #     filepath=args.test_file,
-    #     tokenizer=tokenizer,
-    #     data_fold="val",
-    #     max_seq_length=args.max_seq_length,
-    #     num_processes=6,
-    #     num_samples=None,
-    #     subjects=None,
-    # )
-
     # val_loader = DataLoader(
     #     val_dataset,
     #     shuffle=False,
     #     collate_fn=DataCollatorForCausalLM(tokenizer=tokenizer, max_seq_len=4096),
     #     batch_size=args.per_device_eval_batch_size,
-    # )
-
-    # test_dataset = get_mmlu_open_instruct(
-    #     filepath=args.test_file,
-    #     tokenizer=tokenizer,
-    #     data_fold="test",
-    #     max_seq_length=args.max_seq_length,
-    #     num_processes=6,
-    #     num_samples=None,
-    #     subjects=None,
     # )
 
     # test_loader = DataLoader(
@@ -826,42 +737,130 @@ def main():
 
     # ****************************************************************************
 
-    train_dataset = get_dataset_hf(
-        filepath=args.train_file,
-        data_name=args.dataset_name,
-        tokenizer=tokenizer,
-        max_seq_length=args.max_seq_length,
-        num_processes=6,
-        num_train_examples=None,
-        dataset_info=None,
-    )
-    # longest_seq_length_train, _ = get_longest_seq_length(train_dataset)
-    # model.max_seq_length = longest_seq_length_train
-    # print(
-    #     f"The longest sequence length in the train data is {longest_seq_length_train}, the model's maximum sequence length is"
-    #     f" {model.max_seq_length} and context length is {model.config.block_size}"
+    # train_dataset = get_dataset_hf(
+    #     filepath=args.train_file,
+    #     data_name=args.dataset_name,
+    #     tokenizer=tokenizer,
+    #     max_seq_length=args.max_seq_length,
+    #     num_processes=6,
+    #     num_train_examples=None,
+    #     dataset_info=None,
     # )
+    # # longest_seq_length_train, _ = get_longest_seq_length(train_dataset)
+    # # model.max_seq_length = longest_seq_length_train
+    # # print(
+    # #     f"The longest sequence length in the train data is {longest_seq_length_train}, the model's maximum sequence length is"
+    # #     f" {model.max_seq_length} and context length is {model.config.block_size}"
+    # # )
+    # sys.stdout.flush()
+    # val_dataset = get_mmlu_open_instruct(
+    #     filepath=args.val_file,
+    #     tokenizer=tokenizer,
+    #     data_fold="val",
+    #     max_seq_length=2048,
+    #     num_processes=6,
+    #     num_samples=None,
+    #     subjects=None,
+    # )
+    # test_dataset = None
+    # if args.test_file is not None:
+    #     test_dataset = get_mmlu_open_instruct(
+    #         filepath=args.test_file,
+    #         tokenizer=tokenizer,
+    #         data_fold="test",
+    #         max_seq_length=2048,
+    #         num_processes=6,
+    #         num_samples=None,
+    #         subjects=None,
+    #     )
+
+    # # Preprocessing the datasets.
+
+    if args.dataset_name is not None:
+        # Downloading and loading a dataset from the hub.
+        raw_datasets = load_dataset(
+            args.dataset_name,
+            args.dataset_config_name,
+        )
+    else:
+        data_files = {}
+        dataset_args = {}
+        if args.train_file is not None:
+            data_files["train"] = args.train_file
+        raw_datasets = load_dataset(
+            "json",
+            data_files=data_files,
+            **dataset_args,
+        )
+
+    print("start preprocessing the data. \n\n")
     sys.stdout.flush()
-    val_dataset = get_mmlu_open_instruct(
-        filepath=args.val_file,
+    if (
+        "prompt" in raw_datasets["train"].column_names
+        and "completion" in raw_datasets["train"].column_names
+    ):
+        encode_function = partial(
+            encode_with_prompt_completion_format,
+            tokenizer=tokenizer,
+            max_seq_length=args.max_seq_length,
+        )
+    elif "messages" in raw_datasets["train"].column_names:
+        encode_function = partial(
+            encode_with_messages_format,
+            tokenizer=tokenizer,
+            max_seq_length=args.max_seq_length,
+        )
+    else:
+        raise ValueError(
+            "You need to have either 'prompt'&'completion' or 'messages' in your column names."
+        )
+
+    with accelerator.main_process_first():
+        lm_datasets = raw_datasets.map(
+            encode_function,
+            batched=False,
+            num_proc=args.preprocessing_num_workers,
+            load_from_cache_file=not args.overwrite_cache,
+            remove_columns=[
+                name
+                for name in raw_datasets["train"].column_names
+                if name not in ["input_ids", "labels", "attention_mask"]
+            ],
+            desc="Tokenizing and reformatting instruction data",
+        )
+
+        lm_datasets.set_format(type="pt")
+        lm_datasets = lm_datasets.filter(
+            lambda example: (example["labels"] != -100).any()
+        )
+
+    train_dataset = lm_datasets["train"]
+    print("finished preprocessing. \n\n")
+    sys.stdout.flush()
+
+    # Log a few random samples from the training set:
+    # for index in random.sample(range(len(train_dataset)), 3):
+    #    logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
+
+    val_dataset = get_mmlu_open_instruct_old(
+        filepath=args.test_file,
         tokenizer=tokenizer,
         data_fold="val",
-        max_seq_length=2048,
+        max_seq_length=args.max_seq_length,
         num_processes=6,
         num_samples=None,
         subjects=None,
     )
-    test_dataset = None
-    if args.test_file is not None:
-        test_dataset = get_mmlu_open_instruct(
-            filepath=args.test_file,
-            tokenizer=tokenizer,
-            data_fold="test",
-            max_seq_length=2048,
-            num_processes=6,
-            num_samples=None,
-            subjects=None,
-        )
+
+    test_dataset = get_mmlu_open_instruct_old(
+        filepath=args.test_file,
+        tokenizer=tokenizer,
+        data_fold="test",
+        max_seq_length=args.max_seq_length,
+        num_processes=6,
+        num_samples=None,
+        subjects=None,
+    )
 
     # ******************************************************************************************
 
@@ -1028,15 +1027,15 @@ def main():
     print_memory_consumed()
     print("before train run")
 
-    #acc = evaluate(
+    # acc = evaluate(
     #    model=model,
     #    dataloader=test_loader,
     #    tokenizer=tokenizer,
     #    restrict_targets=True,
-    #)
-    #print(f"baseline average mmlu test accuracy: {acc:.4f}")
-    #print_memory_consumed()
-    #print("before after evaluate")
+    # )
+    # print(f"baseline average mmlu test accuracy: {acc:.4f}")
+    # print_memory_consumed()
+    # print("before after evaluate")
 
     for epoch in range(starting_epoch, args.num_train_epochs):
         acc = evaluate(
