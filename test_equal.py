371ccbbea1fa972bb7e5bf2bac3ab889ac4ab682
diegodoimo <doimo.diego@gmail.com>
2024-04-26 14:51:06 +0000

    u

---
+++ test_equal.py
@@ -0,0 +1,21 @@
+import torch
+import sys
+
+
+
+
+print("loading")
+sys.stdout.flush()
+
+dirpath_actual = '/u/area/ddoimo/ddoimo/finetuning_llm/open-instruct/results/llama-2-7b'
+actual = torch.load(f"{dirpath_actual}/base_model.model.model.layers.1.input_layernorm.pt")
+
+dirpath_original = "/u/area/ddoimo/ddoimo/open/geometric_lens/repo/results/validation/llama-2-7b/0shot"
+expected = torch.load(f"{dirpath_original}/l1_target.pt")
+
+
+print("testing")
+sys.stdout.flush()
+
+torch.testing.assert_close(actual, expected)
+
