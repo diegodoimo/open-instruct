99bc5a1ca2dd4db154123d78c20881e58ec2623a
DiegoD <doimo.diego@gmail.com>
2024-05-04 15:06:08 +0200

    u

--- open_instruct/check_repr_initializations.py
+++ open_instruct/check_repr_initializations.py
@@ -338,14 +338,9 @@ def parse_args():
         help="",
     )
     parser.add_argument(
-        "--train_on_test",
+        "--balance_dataset",
         action="store_true",
         help="",
-    )
-    parser.add_argument(
-        "--mask_path",
-        type="str",
-        default=None,
     )
     parser.add_argument("--overlap_base_dir", type=str, default=None, help="")
 
@@ -376,50 +371,6 @@ def main():
         accelerator_log_kwargs["log_with"] = args.report_to
         accelerator_log_kwargs["project_dir"] = args.output_dir
 
-    # os.environ["ACCELERATE_MIXED_PRECISION"] = args.precision
-
-    # # # we use fsdp also when world size ==1. accelerate issue in casting
-    # if int(os.environ["WORLD_SIZE"]) > 1:
-    #     os.environ["ACCELERATE_USE_FSDP"] = "true"
-
-    # #     os.environ["FSDP_SHRDING_STRATEGY"] = "FULL_SHARD"
-    # #     os.environ["FSDP_AUTO_WRAP_POLICY"] = "TRANSFORMER_BASED_WRAP"
-    # #     os.environ["FSDP_TRANSFORMER_CLS_TO_WRAP"] = "LlamaDecoderLayer"
-
-    # #     os.environ["FSDP_BACKWARD_PREFETCH"] = "BACKWARD_PRE"
-    # #     os.environ["FSDP_STATE_DICT_TYPE"] = "SHARDED_STATE_DICT"
-    # #     os.environ["FSDP_OFFLOAD_PARAMS"] = "false"
-
-    # def lambda_fn(module: torch.nn.Module):
-    #     if isinstance(module, LlamaDecoderLayer):
-    #         return True  # like transformer_auto_wrap_policy
-    #     if isinstance(module, torch.nn.Linear) and all(
-    #         p.requires_grad for p in module.parameters()
-    #     ):
-    #         return True  # wrap each trainable linear separately
-    #     return False
-
-    # auto_wrap_policy = partial(lambda_auto_wrap_policy, lambda_fn=lambda_fn)
-
-    # fsdp_plugin = FullyShardedDataParallelPlugin(
-    #     sharding_strategy=ShardingStrategy.FULL_SHARD,
-    #     backward_prefetch=BackwardPrefetch.BACKWARD_PRE,
-    #     mixed_precision_policy=MixedPrecision(
-    #         param_dtype=torch.bfloat16,
-    #         reduce_dtype=torch.bfloat16,
-    #         buffer_dtype=torch.bfloat16,
-    #     ),
-    #     auto_wrap_policy=auto_wrap_policy,
-    #     cpu_offload=False,
-    #     ignored_modules=None,
-    #     limit_all_gathers=True,
-    #     use_orig_params=False,
-    #     param_init_fn=None,
-    #     sync_module_states=True,
-    #     forward_prefetch=False,
-    #     activation_checkpointing=False,
-    # )
-
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         **accelerator_log_kwargs,
@@ -446,11 +397,11 @@ def main():
     if accelerator.is_main_process:
         if args.train_on_dev:
             args.output_dir += "/dev"
-        elif args.train_on_test:
-            args.output_dir += "/test_balanced"
-        else:
+        elif args.balance_dataset:
             args.output_dir += "/dev_val_balanced"
-        args.output_dir += f"/{args.num_train_epochs}epochs"
+        else:
+            args.output_dir += "/dev_val"
+        args.output_dir += f"/{args.num_train_epochs}"
         os.makedirs(args.output_dir, exist_ok=True)
 
     accelerator.wait_for_everyone()
@@ -503,61 +454,6 @@ def main():
         max_seq_len = args.max_seq_length
     accelerator.print(max_seq_len)
 
-    # ****************************************************************************
-    # # Preprocessing the datasets.
-
-    # train_dataset = get_dataset_open_instruct_new(
-    #     accelerator=accelerator,
-    #     filepath=args.train_file,
-    #     tokenizer=tokenizer,
-    #     max_seq_length=2048,
-    #     num_processes=1,
-    # )
-
-    # val_dataset = get_mmlu_open_instruct(
-    #     filepath=args.test_file,
-    #     tokenizer=tokenizer,
-    #     data_fold="val",
-    #     max_seq_length=args.max_seq_length,
-    #     num_processes=6,
-    #     num_samples=None,
-    #     subjects=None,
-    # )
-
-    # test_dataset = get_mmlu_open_instruct(
-    #     filepath=args.test_file,
-    #     tokenizer=tokenizer,
-    #     data_fold="test",
-    #     max_seq_length=args.max_seq_length,
-    #     num_processes=6,
-    #     num_samples=None,
-    #     subjects=None,
-    # )
-
-    train_dataset, longest_seq = MMLU_Dataset(
-        tokenizer=tokenizer,
-        max_seq_len=args.max_seq_length,
-        num_few_shots=0,
-        accelerator=accelerator,
-        subject=None,
-        num_processes=args.preprocessing_num_workers,
-        split="train",
-        train_on_dev=args.train_on_dev,
-        train_on_test=args.train_on_test,
-        mask_path=args.mask_path,
-    ).construct_dataset()
-    accelerator.print(f"num_samples = {len(train_dataset)}")
-
-    val_dataset, longest_seq = MMLU_Dataset(
-        tokenizer=tokenizer,
-        max_seq_len=args.max_seq_length,
-        num_few_shots=0,
-        accelerator=accelerator,
-        subject=None,
-        num_processes=args.preprocessing_num_workers,
-        split="validation",
-    ).construct_dataset()
-
     test_dataset, longest_seq = MMLU_Dataset(
         tokenizer=tokenizer,
         max_seq_len=args.max_seq_length,
@@ -573,25 +469,6 @@ def main():
     assert args.per_device_train_batch_size == 1
     assert args.per_device_eval_batch_size == 1
 
-    # # DataLoaders creation:
-    train_loader, train_sampler = get_dataloader(
-        train_dataset,
-        args.per_device_train_batch_size,
-        tokenizer.pad_token_id,
-        world_size=world_size,
-        shuffle=True,
-        num_processes=6,
-        return_sampler=True,
-    )
-
-    val_loader = get_dataloader(
-        val_dataset,
-        args.per_device_eval_batch_size,
-        tokenizer.pad_token_id,
-        world_size=world_size,
-        shuffle=False,
-        num_processes=6,
-    )
     test_loader = None
     if test_dataset is not None:
         test_loader = get_dataloader(
@@ -603,283 +480,16 @@ def main():
             num_processes=6,
         )
 
-    # *******************************************************************************
-
-    gradient_accumulation_iters = max(
-        1, int(args.batch_size / args.per_device_train_batch_size / world_size)
-    )
-    optimizer = get_optimizer(
-        model=model,
-        learning_rate=args.learning_rate,
-        weight_decay=args.weight_decay,
-    )
-
-    lr_scheduler = get_scheduler(
-        args.lr_scheduler_type,
-        optimizer,
-        epochs=args.num_train_epochs,
-        num_iters=len(train_loader),
-        warmup_steps=args.warmup_steps,
-        warmup_ratio=args.warmup_ratio,
-        gradient_accumulation_iters=gradient_accumulation_iters,
-    )
-
     # ************************************************************************
 
     # Prepare everything with `accelerator`.
     model = accelerator.prepare(model)
-    optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-        optimizer, train_loader, lr_scheduler
-    )
-
-    # We need to recalculate our total training steps as the size of the training dataloader may have changed.
-    num_update_steps_per_epoch = math.ceil(
-        len(train_dataloader) / args.gradient_accumulation_steps
-    )
-
-    args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
-
-    # Afterwards we recalculate our number of training epochs
-    args.num_train_epochs = math.ceil(args.max_train_steps / num_update_steps_per_epoch)
-
-    # Figure out how many steps we should save the Accelerator states
-    checkpointing_steps = args.checkpointing_steps
-    if checkpointing_steps is not None and checkpointing_steps.isdigit():
-        checkpointing_steps = int(checkpointing_steps)
-
-    # We need to initialize the trackers we use, and also store our configuration.
-    # The trackers initializes automatically on the main process.
-    if args.with_tracking:
-        experiment_config = vars(args)
-        # TensorBoard cannot log Enums, need the raw value
-        experiment_config["lr_scheduler_type"] = experiment_config[
-            "lr_scheduler_type"
-        ].value
-        accelerator.init_trackers("open_instruct", experiment_config)
-
-    # ****************************************************************************************
-
-    # ****************************************************************************************
-    # Train!
-    total_batch_size = (
-        args.per_device_train_batch_size
-        * accelerator.num_processes
-        * args.gradient_accumulation_steps
-    )
-
-    logger.info("***** Running training *****")
-    logger.info(f"  Num examples = {len(train_dataset)}")
-    logger.info(f"  Num Epochs = {args.num_train_epochs}")
-    logger.info(
-        f"  Instantaneous batch size per device = {args.per_device_train_batch_size}"
-    )
-    logger.info(
-        f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}"
-    )
-    logger.info(f"  Gradient Accumulation steps = {args.gradient_accumulation_steps}")
-    logger.info(f"  Total optimization steps = {args.max_train_steps}")
-
-    # ****************************************************************************************
-    completed_steps = 0
-    starting_epoch = 0
 
     # save pretrained model for double check
     output_dir = f"epoch_0"
     if args.output_dir is not None:
         output_dir = os.path.join(args.output_dir, output_dir)
     save_with_accelerate(accelerator, model, output_dir, args)
-
-    filename = ""
-    if args.out_filename != "":
-        filename = "_" + args.out_filename
-
-    stats = defaultdict()
-    stats["num_epochs"] = args.num_train_epochs
-    stats["lr"] = args.learning_rate
-    stats["scheduler"] = args.lr_scheduler_type
-    stats["batch_size"] = args.batch_size
-
-    meter = measure_statistics(
-        stats,
-        model,
-        val_loader,
-        test_loader,
-        tokenizer,
-        accelerator,
-        ckpt_dir=args.overlap_base_dir,
-        results_dir=args.output_dir,
-        prepare_for_overlap=args.measure_overlap,
-        filename=f"{filename}epoch{args.num_train_epochs}",
-    )
-
-    if args.measure_baselines:
-        meter.update(
-            accelerator=accelerator,
-            model=model,
-            completed_steps=0,
-            epoch=0,
-            do_overlap=args.measure_overlap,
-        )
-
-    accelerator.print("start training")
-    print_memory_consumed()
-    accelerator.print("before train run")
-    sys.stdout.flush()
-
-    # *******************************************************************************
-
-    for epoch in range(starting_epoch, args.num_train_epochs):
-        meter.update(
-            accelerator=accelerator,
-            model=model,
-            completed_steps=0,
-            epoch=0,
-            do_overlap=args.measure_overlap,
-            do_val=True,
-        )
-
-        model.train()
-        total_loss = 0
-        active_dataloader = train_dataloader
-
-        start = time.time()
-        for step, batch in enumerate(active_dataloader):
-            with accelerator.accumulate(model):
-
-                outputs = model(**batch, use_cache=False)
-                loss = outputs.loss
-                # We keep track of the loss at each logged step
-                total_loss += loss.detach().float()
-                accelerator.backward(loss)
-                # clip gradient norm. don't do this with deepspeed
-                if accelerator.sync_gradients and args.clip_grad_norm > 0:
-                    accelerator.clip_grad_norm_(model.parameters(), args.clip_grad_norm)
-                optimizer.step()
-                optimizer.zero_grad()
-                lr_scheduler.step()
-
-            # Checks if the accelerator has performed an optimization step behind the scenes
-            if accelerator.sync_gradients:
-                # progress_bar.update(1)
-                completed_steps += 1
-                if args.logging_steps and completed_steps % args.eval_steps == 0:
-                    t_tot = time.time() - start
-
-                    avg_loss = (
-                        accelerator.gather(total_loss).mean().item()
-                        / args.gradient_accumulation_steps
-                        / args.logging_steps
-                    )
-                    logger.info(
-                        f"  Step: {completed_steps}, LR: {lr_scheduler.get_last_lr()[0]}, Loss: {avg_loss}, Time: {t_tot/3600: .2f} hours"
-                    )
-                    total_loss = 0
-
-                    # if completed_steps % args.eval_steps == 0:
-
-                    meter.update(
-                        accelerator=accelerator,
-                        model=model,
-                        loss=avg_loss,
-                        completed_steps=completed_steps,
-                        epoch=epoch,
-                        do_val=True,
-                        do_overlap=args.measure_overlap,
-                    )
-
-                if isinstance(checkpointing_steps, int):
-                    if completed_steps % checkpointing_steps == 0:
-                        output_dir = f"step_{completed_steps}"
-                        if args.output_dir is not None:
-                            output_dir = os.path.join(args.output_dir, output_dir)
-                        save_with_accelerate(accelerator, model, output_dir, args)
-
-                if completed_steps >= args.max_train_steps:
-                    break
-
-        meter.update(
-            accelerator=accelerator,
-            model=model,
-            completed_steps=completed_steps,
-            epoch=epoch,
-            do_test=True,
-            do_overlap=args.measure_overlap,
-        )
-        print_memory_consumed()
-
-        # save model
-        output_dir = f"epoch_{epoch+1}"
-        if args.output_dir is not None:
-            output_dir = os.path.join(args.output_dir, output_dir)
-        save_with_accelerate(accelerator, model, output_dir, args)
-
-    if args.with_tracking:
-        accelerator.end_training()
-
-    # if args.output_dir is not None:
-    #     accelerator.wait_for_everyone()
-    #     if accelerator.is_main_process:
-    #         tokenizer.save_pretrained(args.output_dir)
-    #     save_with_accelerate(accelerator, model, args.output_dir, args)
-
-
-# FSDP has issues with `inference_mode`
-# @torch.inference_mode()
-@torch.no_grad()
-def evaluate(model, dataloader, tokenizer, restrict_targets):
-    model.eval()
-
-    predictions, ground_truths = [], []
-    choices = ["A", "B", "C", "D"]
-
-    candidate_token_ids = None
-    if restrict_targets:
-        candidate_token_ids = [
-            tokenizer.encode(" " + answer_choice, add_special_tokens=False)[-1]
-            for answer_choice in choices
-        ]
-
-    for iter_num, batch in enumerate(dataloader):
-        if (iter_num + 1) % int(2000 / dataloader.batch_size) == 0:
-            print(
-                f"{iter_num * dataloader.batch_size+1}/ {len(dataloader.dataset)} inputs processed"
-            )
-            sys.stdout.flush()
-
-        input_ids, targets, mask = (
-            batch["input_ids"],
-            batch["labels"],
-            batch["attention_mask"],
-        )
-        input_ids = input_ids.to("cuda")
-        outputs = model(input_ids)
-        logits = outputs.logits
-
-        seq_len = torch.sum(mask, dim=1)
-        batch_probs = torch.softmax(
-            logits[torch.arange(input_ids.shape[0]), seq_len - 1, :], dim=-1
-        )
-        if candidate_token_ids is not None:
-            batch_probs = batch_probs[:, candidate_token_ids]
-        batch_prediction_indices = torch.argmax(batch_probs, dim=-1)
-        predictions += batch_prediction_indices.tolist()
-        ground_truths += tokenizer.batch_decode(targets, skip_special_tokens=True)
-
-    assert len(predictions) == len(
-        dataloader.dataset
-    ), "number of predictions should be equal to number of prompts"
-
-    # get the metrics
-    cors = []
-    for i in range(len(predictions)):
-        prediction = choices[predictions[i]]
-        ground_truth = ground_truths[i]
-        cors.append(prediction == ground_truth)
-
-    acc = np.mean(cors)
-    model.train()
-
-    return acc
 
 
 class measure_statistics:
@@ -1019,6 +629,14 @@ class measure_statistics:
                 logger.info(
                     f"iter {completed_steps}. overlap with subjects outputs {shot}: {list(overlaps[shot].values())[-1]}\n"
                 )
+                # for norm, norm_val in shot_val.items():
+                #     for k, k_val in norm_val.items():
+                #         logger.info(
+                #             f"iter {completed_steps}. overlap with subjects outputs {shot}, {norm}, {k}: {list(overlaps[shot][norm][k].values())[-1]}\n"
+                #         )
+                #         logger.info(
+                #             f"iter {completed_steps}. overlap outputs {shot}, {norm}, {k}: {np.mean(list(list(overlaps[shot][norm][k].values())[-1].values())):.4f}\n"
+                #         )
 
         self.stats["train_stats"] = self.train_stats
         with open(
